36250b2c4f3c3238e7daa31790b0e79865c9cc0b
Kevin Quach <kevin.quach@gmail.com>
2014-02-17 18:25:54 -0500

    change LSadapter to FixtureAdapter

--- js/application.js
+++ js/application.js
@@ -2,6 +2,8 @@ window.Todos = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
-Todos.ApplicationAdapter = DS.LSAdapter.extend({
-  namespace: 'todos-emberjs'
-});
+// Todos.ApplicationAdapter = DS.LSAdapter.extend({
+//  namespace: 'todos-emberjs'
+// });
+
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
